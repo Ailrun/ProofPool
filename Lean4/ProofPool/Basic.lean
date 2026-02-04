14b5a4817dde136663de46ec4a1e8a53957057d2
Junyoung/Clare Jang <jjc9310@gmail.com>
2026-02-04 00:19:51 -0500

    Prove some quotient things

--- Lean4/ProofPool/Basic.lean
+++ Lean4/ProofPool/Basic.lean
@@ -19,23 +19,50 @@ def MyInt.mk (n : MyInt') : MyInt := Quotient.mk _ n
 instance : OfNat MyInt n where
   ofNat := MyInt.mk (n, 0)
 
+def MyInt.neg' (n : MyInt') : MyInt := .mk (n.2, n.1)
+
+instance : Neg MyInt where
+  neg (n : MyInt) := n.lift MyInt.neg' <| by
+    intro (a₁, a₂) (a'₁, a'₂) heq
+    apply Quotient.sound
+    simp only [· ≈ ·, Setoid.r, MyInt.eq] at *
+    omega
+
 def MyInt.add' (n k : MyInt') : MyInt := .mk (n.1 + k.1, n.2 + k.2)
 
 instance : Add MyInt where
   add (n : MyInt) := n.lift₂ MyInt.add' <| by
-    intro a b a' b' heq heq'
+    intro (a₁, a₂) (b₁, b₂) (a'₁, a'₂) (b'₁, b'₂) heq heq'
     apply Quotient.sound
-    cases a; cases b; cases a'; cases b'
     simp_all only [· ≈ ·, Setoid.r, MyInt.eq]
     omega
 
--- (a, b) * (c, d) = (ac + bd, ad + bc)
 def MyInt.mul' (n k : MyInt') : MyInt := .mk (n.1 * k.1 + n.2 * k.2, n.1 * k.2 + n.2 * k.1)
 
--- instance : Mul MyInt where
---   mul (n : MyInt) := n.lift₂ MyInt.mul' <| by
---     intro a b a' b' heq heq'
---     apply Quotient.sound
---     cases a; cases b; cases a'; cases b'
---     simp_all only [· ≈ ·, Setoid.r, MyInt.eq]
---     omega
+instance : Mul MyInt where
+  mul (n : MyInt) := n.lift₂ MyInt.mul' <| by
+    intro (a₁, a₂) (b₁, b₂) (a'₁, a'₂) (b'₁, b'₂) heq heq'
+    calc MyInt.mul' (a₁, a₂) (b₁, b₂)
+      _ = MyInt.mul' (a'₁, a'₂) (b₁, b₂)   := by
+        apply Quotient.sound
+        simp_all only [· ≈ ·, Setoid.r, MyInt.eq]
+        calc a₁ * b₁ + a₂ * b₂ + (a'₁ * b₂ + a'₂ * b₁)
+          _ = (a₁ + a'₂) * b₁ + (a₂ + a'₁) * b₂         := by lia
+          _ = (a₂ + a'₁) * b₁ + (a₁ + a'₂) * b₂         := by simp_all
+          _ = a₁ * b₂ + a₂ * b₁ + (a'₁ * b₁ + a'₂ * b₂) := by lia
+      _ = MyInt.mul' (a'₁, a'₂) (b'₁, b'₂) := by
+        apply Quotient.sound
+        simp_all only [· ≈ ·, Setoid.r, MyInt.eq]
+        calc a'₁ * b₁ + a'₂ * b₂ + (a'₁ * b'₂ + a'₂ * b'₁)
+          _ = a'₁ * (b₁ + b'₂) + a'₂ * (b₂ + b'₁)           := by lia
+          _ = a'₁ * (b₂ + b'₁) + a'₂ * (b₁ + b'₂)           := by simp_all
+          _ = a'₁ * b₂ + a'₂ * b₁ + (a'₁ * b'₁ + a'₂ * b'₂) := by lia
+
+def MyInt.sub' (n k : MyInt') : MyInt := .mk (n.1 + k.2, n.2 + k.1)
+
+instance : Sub MyInt where
+  sub (n : MyInt) := n.lift₂ MyInt.sub' <| by
+    intro (a₁, a₂) (b₁, b₂) (a'₁, a'₂) (b'₁, b'₂) heq heq'
+    apply Quotient.sound
+    simp_all only [· ≈ ·, Setoid.r, MyInt.eq]
+    omega
